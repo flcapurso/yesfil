aef58c9a05c3944eae2a00529f7629a03a063ade
Filippo Capurso <flcapurso@gmail.com>
2025-05-07 22:53:20 -0600

    Finally v1.0 ready! - added polarsteps link - changed invitation font - re-added eucalyptus - handled plural+singular f/m in italian

--- invitation/script.js
+++ invitation/script.js
@@ -7,8 +7,21 @@ $(document).ready(function() {
   let searchParams = new URLSearchParams(window.location.search)
   let names = searchParams.get('names')
   let secret = searchParams.get('secret')
+  const lang = location.href.split("/").slice(-1)[0].split(".")[0]
   console.log(names)
   console.log(secret)
+
+  if (lang == "ita") {
+    if (names.includes(",")) {
+      names = "Cari " + names
+    }
+    else if (names.slice(-1) == "a") {
+      names = "Cara " + names
+    }
+    else {
+      names = "Caro " + names
+    }
+  }
 
   if (names) {
     $('#names').text(names)
